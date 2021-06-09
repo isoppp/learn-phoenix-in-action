b49cf268376dbbd93ae88cf701e2303e93fc8a94
isoppp <prog@d2deck.com>
2021-06-09 13:49:11 +0900

    refactor fizzbuzz using cond

--- fizzbazz.ex
+++ fizzbazz.ex
@@ -1,13 +1,14 @@
 defmodule FizzBuzz do
-  def go(min, max) do
-    min..max
-    |> Enum.each(fn num -> go(num) end)
-  end
+  def go(min, max), do: Enum.each(min..max, &go(&1))
 
-  def go(num) when rem(num, 15) == 0, do: IO.puts("fizzbuzz")
-  def go(num) when rem(num, 3) == 0, do: IO.puts("fizz")
-  def go(num) when rem(num, 5) == 0, do: IO.puts("buzz")
-  def go(num), do: IO.puts(num)
+  def go(num) do
+    cond do
+      rem(num, 15) == 0 -> IO.puts("fizzbuzz")
+      rem(num, 3) == 0 -> IO.puts("fizz")
+      rem(num, 5) == 0 -> IO.puts("buzz")
+      true -> IO.puts(num)
+    end
+  end
 end
 
 FizzBuzz.go(1, 100)
