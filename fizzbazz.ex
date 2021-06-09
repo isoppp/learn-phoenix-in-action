f57f8c00101c8f13bc25395b9e7f251115050332
isoppp <prog@d2deck.com>
2021-06-09 13:42:20 +0900

    add first code fizzbuzz

---
+++ fizzbazz.ex
@@ -0,0 +1,13 @@
+defmodule FizzBuzz do
+  def go(min, max) do
+    min..max
+    |> Enum.each(fn num -> go(num) end)
+  end
+
+  def go(num) when rem(num, 15) == 0, do: IO.puts("fizzbuzz")
+  def go(num) when rem(num, 3) == 0, do: IO.puts("fizz")
+  def go(num) when rem(num, 5) == 0, do: IO.puts("buzz")
+  def go(num), do: IO.puts(num)
+end
+
+FizzBuzz.go(1, 100)
