666d6b0b150d19a12b9051151137eeb9ed5008a8
isoppp <prog@d2deck.com>
2021-06-18 15:10:05 +0900

    C4: add default value to Auction.Item struct

--- auction/item.ex
+++ auction/item.ex
@@ -1,3 +1,6 @@
 defmodule Auction.Item do
-  defstruct [:id, :title, :description, :ends_at]
+  defstruct id: 0,
+            title: "default title",
+            description: "default description",
+            ends_at: ~N[2020-12-31 23:59:59]
 end
