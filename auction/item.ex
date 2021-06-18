e4eaab796203f1f6c0b4fce3c3f4aa878dfb2ca8
isoppp <prog@d2deck.com>
2021-06-18 15:39:00 +0900

    C4: Implemented full Acution module in one file

--- auction/item.ex
+++
@@ -1,6 +0,0 @@
-defmodule Auction.Item do
-  defstruct id: 0,
-            title: "default title",
-            description: "default description",
-            ends_at: ~N[2020-12-31 23:59:59]
-end
